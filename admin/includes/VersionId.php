6f44fb501abcdf105324ae5b993a685043da846b
ThomasFinnern <noreply@tomfinnern.de>
2016-01-24 23:17:42 +0100

    Tested version compare

--- admin/includes/VersionId.php
+++ admin/includes/VersionId.php
@@ -10,8 +10,29 @@
 // no direct access
 //defined('_JEXEC') or die;
 
-class VersionId {
-	public $IdParts = array ();
+interface Comparable
+{
+	/**
+	 * @param object $object
+	 * @return int negative value if $this < $object, positive if $this > $object, 0 otherwise (if objects are considered equal)
+	 * @throws ComparatorException if objects are not comparable to each other
+	 */
+	public function compareTo($object);
+}
+
+interface Comparator
+{
+	/**
+	 * @param object $object1
+	 * @param object $object2
+	 * @return int Negative value if $object1 < $object2, positive if $object1 > $object2, 0 otherwise
+	 * @throws ComparatorException if objects are not comparable to each other
+	 */
+	public static function compare($object1, $object2);
+}
+
+class VersionId implements Comparator {
+	protected $IdParts = array ();
 	
 	public function  __construct ($strId) {
 		$this->AssignId ($strId);
@@ -24,31 +45,35 @@ class VersionId {
 		}
 	}
 	
-	
 	public function Count () {
 		return count ($this->IdParts);
 	}
 	
-	public function Compare ($otherId)
+	public static function Compare ($firstId, $secondId)
 	{
-		$LengthId = count($this->IdParts);
-		$LengthOther = $otherId->Count();
+		$LengthFirst = count($firstId->IdParts);
+		$LengthSecond = $secondId->Count();
 			
-		$Length = $LengthId;
-		if ($Length > $LengthOther) {
-			$Length = $LengthOther;
+		$Length = $LengthFirst;
+		if ($Length > $LengthSecond) {
+			$Length = $LengthSecond;
 		}
 		
 		// Compare array elements 
 		for ($Idx = 0; $Idx < $Length; $Idx++) {
-			if($this->IdParts[$Idx] != $otherId->IdParts[$Idx]) {
-				return $this->IdParts[$Idx] > $otherId->IdParts[$Idx];
+			if($firstId->IdParts[$Idx] != $secondId->IdParts[$Idx]) {
+				//return $firstId->IdParts[$Idx] > $secondId->IdParts[$Idx];
+                $a = $firstId->IdParts[$Idx];
+                $b = $secondId->IdParts[$Idx];
+                return ($a < $b) ? -1 : (($a > $b) ? 1 : 0);
 			}
 		}
 		
 		// compare length 
-		return $LengthId > $LengthOther;		
-	}	
+        $a = $LengthFirst;
+        $b = $LengthSecond;
+        return ($a < $b) ? -1 : (($a > $b) ? 1 : 0);
+	}
 		
 		
 	
