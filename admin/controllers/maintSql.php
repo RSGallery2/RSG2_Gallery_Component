02a0103f26de2409dc75042bbf83d5ebb07b7c70
Thomas.Finnern <GitProjects@insidethemachine.de>
2016-04-18 22:52:22 +0200

    Added comment

--- admin/controllers/maintSql.php
+++ admin/controllers/maintSql.php
@@ -118,6 +118,19 @@ class Rsgallery2ControllerMaintSql extends JControllerAdmin
 		$msg = "createGalleryAccessField: ";
 		$msgType = 'notice';
 
+		/*
+			1054 Unknown column 'access' in 'field list' SQL=INSERT INTO `afs_rsgallery2_galleries`
+		         (`id`,`parent`,`name`,`alias`,`description`,`published`,`checked_out`,
+		          `checked_out_time`,`ordering`,`hits`,`date`,`params`,`user`,`uid`,`allowed`,
+		          `thumb_id`,`access`) VALUES ('','0','fdtgsdg','fdtgsdg','','1','','','0','',
+		          '2015-10-01 16:18:23','','','45','','','1')
+
+			After that i simply go through to afs_rsgallery2_galleries table and create a new field
+		    called "access" and then set then value "1" for access via update query.
+		    And my gallery images make a path or url...
+		*/
+
+
 		$msg .= '!!! Not implemented yet !!!';
 
 		$this->setRedirect('index.php?option=com_rsgallery2&view=maintenance', $msg, $msgType);
