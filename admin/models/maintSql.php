b5fce55b99ecf746d366ac21d711c52c08f08a5e
ThomasFinnern <noreplay@tomfinnern.de>
2016-04-28 12:53:55 +0200

    Improved table list, not ready

--- admin/models/maintSql.php
+++ admin/models/maintSql.php
@@ -15,7 +15,7 @@ class Rsgallery2ModelMaintSql extends  JModelList
 {
 //    protected $text_prefix = 'COM_RSG2';
 
-
+	protected $tableList;
 	/**
 	 *
 	 */
@@ -67,11 +67,8 @@ class Rsgallery2ModelMaintSql extends  JModelList
 
 		// !!! test code -> delete actual column (field)
 		if ($ColumnExist) {
-			// ALTER TABLE t2 DROP COLUMN c, DROP COLUMN d;
-			$query = 'ALTER TABLE ' . $table . ' DROP COLUMN ' . $ColumnName ;
-			$msg .= '<br>' . '$query: ' . json_encode ($query);
-			$db->setQuery($query);
-			$result = $db->execute();
+
+			$result = $this->DeleteColumn($table, $ColumnName);
 			$msg .= '<br>' . '$result (drop): ' . json_encode ($result);
 
 			$ColumnExist = false;
@@ -88,6 +85,8 @@ class Rsgallery2ModelMaintSql extends  JModelList
 		// Set all access values to '1'
 		if ($ColumnExist)
 		{
+
+			$db = JFactory::getDbo();
 			// update your_table set likes = null
 			$query = 'UPDATE ' . $table . ' SET ' . $ColumnName . '=1';
 			$msg .= '<br>' . '$query: ' . json_encode ($query);
@@ -100,7 +99,20 @@ class Rsgallery2ModelMaintSql extends  JModelList
 
 
 	// *
-	public function IsColumnExisting($table, $ColumnName)
+	private function DeleteColumn($table, $ColumnName)
+	{
+		$db = JFactory::getDbo();
+		// ALTER TABLE t2 DROP COLUMN c, DROP COLUMN d;
+		$query = 'ALTER TABLE ' . $table . ' DROP COLUMN ' . $ColumnName ;
+		// $msg .= '<br>' . '$query: ' . json_encode ($query);
+		$db->setQuery($query);
+		$result = $db->execute();
+
+		return $result;
+	}
+
+	// *
+	private function IsColumnExisting($table, $ColumnName)
 	{
 		$IsColumnExisting = false;
 
@@ -112,7 +124,7 @@ class Rsgallery2ModelMaintSql extends  JModelList
 		$IsColumnExisting = isset($AccessField);
 		// $msg .= '<br>' . '$ColumnExist: ' . json_encode ($ColumnExist);
 
-		retrun $IsColumnExisting;
+		return $IsColumnExisting;
 	}
 
 	// *
@@ -140,16 +152,18 @@ class Rsgallery2ModelMaintSql extends  JModelList
 	 */
 	private function getTableListFromSqlFile()
 	{
-		$TableList = array(
-			'#__rsgallery2_galleries',
-			'#__rsgallery2_files',
-			'#__rsgallery2_comments',
-			'#__rsgallery2_config',
-			'#__rsgallery2_acl');
+		// Create only once
+		if (empty ($this->tableList)) {
+			// ToDo: Read file to auto use in the future
+			$this->tableList = array(
+				'#__rsgallery2_galleries',
+				'#__rsgallery2_files',
+				'#__rsgallery2_comments',
+				'#__rsgallery2_config',
+				'#__rsgallery2_acl');
+		}
 
-		// Read file to auto use in the future
-
-		return $TableList;
+		return $this->tableList;
 	}
 
 
@@ -157,39 +171,24 @@ class Rsgallery2ModelMaintSql extends  JModelList
 	{
 		$msg = 'model:completeSqlTables: ' . '<br>';
 
-		/*
+		//--- Check for not existing tables and create them -----------
+		$msg .= $this->createMissingSqlTables ();
+
+		//--- Check for not existing tables and create them --------
 		$tables = $this->getTableListFromSqlFile();
-
-		$db = JFactory::getDbo();
-
-		//--- optimize all tables -------------------------------
-
 		foreach ($tables as $table) {
-			$msg .= 'Table ' . $table . '<br>';
-			$db->setQuery('OPTIMIZE TABLE ' . $db->quoteName($table));
-			$db->execute();
+			$msg .= $this->createMissingSqlFieldsinTable($table);
 		}
-
-		//--- optimized message -------------------------------------
-		$msg .=  '<br>' . JText::_('COM_RSGALLERY2_MAINT_OPTIMIZE_SUCCESS', true );
-		/**/
-
-		$this->createMissingSqlFields ();
-
+		
 		$msg .= '!!! Not implemented yet !!!' . '<br>';
-
 
 		return $msg;
 	}
 
 
-
-
-
-
-	public function createMissingSqlFields()
+	public function createMissingSqlTables()
 	{
-		$msg = "Model: createMissingSqlFields: " . '<br>';
+		$msg = "Model: createMissingSqlTables: " . '<br>';
 		// $msg = '';
 
 		// Original table names
@@ -210,29 +209,33 @@ class Rsgallery2ModelMaintSql extends  JModelList
 		}
 
 		$msg .= 'Check for missing files (columns) in tables<br>';
+		
+		return $msg;
+	}
+	public function createMissingSqlFieldsinTable($table)
+	{
+		$msg = "Model: createMissingSqlFields: " . '   Table ' . $table . '<br>';
 
-		//--- create not exisitn columns if not exist -----------------
+		$msg .= 'Check for missing COLUMN IN TABLE <br>';
 
-		foreach ($tables as $table) {
-			$msg .= '   Table ' . $table . '<br>';
+		//--- create not existing columns if not exist -----------------
 
-			// Original columns
-			$columns = $this->getColumnsPropertiesOfTable($table);
+        // Original columns
+        $columns = $this->getColumnsPropertiesOfTable($table);
 
-			// Create all not existing table columns
-			foreach ($columns as $ColumnName => $ColumnProperties)
-			{
-				// Create table column
-				$ColumnExist = $this->IsColumnExisting($table, $ColumnName);
-				if (!$ColumnExist)
-				{
-					$msg .= $this->createNotExistingColumn($table, $ColumnName, $ColumnProperties, $ColumnExist);
-					if (!$ColumnExist) {
-						$msg .= '   failed to create ' . $table . ':' . $ColumnName . '<br>';
-					}
-				}
-			}
-		}
+        // Create all not existing table columns
+        foreach ($columns as $ColumnName => $ColumnProperties)
+        {
+            // Create table column
+            $ColumnExist = $this->IsColumnExisting($table, $ColumnName);
+            if (!$ColumnExist)
+            {
+                $msg .= $this->createNotExistingColumn($table, $ColumnName, $ColumnProperties, $ColumnExist);
+                if (!$ColumnExist) {
+                    $msg .= '   failed to create ' . $table . ':' . $ColumnName . '<br>';
+                }
+            }
+        }
 
 		return $msg;
 	}
@@ -253,13 +256,99 @@ class Rsgallery2ModelMaintSql extends  JModelList
 
 //		$ColumnName, $ColumnProperties
 
-
-//		return $msg;
-
 		$ColumnsProperties = array ();
 
 		// Test data
 		$ColumnsProperties['access'] = 'INT  (10) UNSIGNED DEFAULT NULL';
+
+
+        switch ($table) {
+            case '#__rsgallery2_galleries':
+
+                `id` int(11) NOT NULL auto_increment,
+  `parent` int(11) NOT NULL default 0,
+  `name` varchar(255) NOT NULL default '',
+  `alias` varchar(255) NOT NULL DEFAULT '',
+  `description` text NOT NULL,
+  `published` tinyint(1) NOT NULL default '0',
+  `checked_out` int(11) unsigned NOT NULL default '0',
+  `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `ordering` int(11) NOT NULL default '0',
+  `date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `hits` int(11) NOT NULL default '0',
+  `params` text NOT NULL,
+  `user` tinyint(4) NOT NULL default '0',
+  `uid` int(11) unsigned NOT NULL default '0',
+  `allowed` varchar(100) NOT NULL default '0',
+  `thumb_id` int(11) unsigned NOT NULL default '0',
+  `asset_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'FK to the #__assets table.',
+  `access` int(10) unsigned DEFAULT NULL,
+                break;
+            case '#__rsgallery2_files':
+                `name` varchar(255) NOT NULL default '',
+  `alias` varchar(255) NOT NULL DEFAULT '',
+  `descr` text,
+  `gallery_id` int(9) unsigned NOT NULL default '0',
+  `title` varchar(255) NOT NULL default '',
+  `hits` int(11) unsigned NOT NULL default '0',
+  `date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `rating` int(10) unsigned NOT NULL default '0',
+  `votes` int(10) unsigned NOT NULL default '0',
+  `comments` int(10) unsigned NOT NULL default '0',
+  `published` tinyint(1) NOT NULL default '1',
+  `checked_out` int(11) NOT NULL default '0',
+  `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `ordering` int(9) unsigned NOT NULL default '0',
+  `approved` tinyint(1) unsigned NOT NULL default '1',
+  `userid` int(10) NOT NULL,
+  `params` text NOT NULL,
+  `asset_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'FK to the #__assets table.',
+                break;
+            case '#__rsgallery2_comments':
+                `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_ip` varchar(50) NOT NULL default '0.0.0.0',
+  `parent_id` int(11) NOT NULL default '0',
+  `item_id` int(11) NOT NULL,
+  `item_table` varchar(50) default NULL,
+  `datetime` datetime NOT NULL,
+  `subject` varchar(100) default NULL,
+  `comment` text NOT NULL,
+  `published` tinyint(1) NOT NULL default '1',
+  `checked_out` int(11) default NULL,
+  `checked_out_time` datetime default NULL,
+  `ordering` int(11) NOT NULL,
+  `params` text,
+  `hits` int(11) NOT NULL,
+                break;
+            case '#__rsgallery2_config':
+                `name` text NOT NULL,
+                  `value` text NOT NULL,
+                break;
+            case '#__rsgallery2_acl':
+                `gallery_id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL default '0',
+  `public_view` tinyint(1) NOT NULL default '1',
+  `public_up_mod_img` tinyint(1) NOT NULL default '0',
+  `public_del_img` tinyint(1) NOT NULL default '0',
+  `public_create_mod_gal` tinyint(1) NOT NULL default '0',
+  `public_del_gal` tinyint(1) NOT NULL default '0',
+  `public_vote_view` tinyint( 1 ) NOT NULL default '1',
+  `public_vote_vote` tinyint( 1 ) NOT NULL default '0',
+  `registered_view` tinyint(1) NOT NULL default '1',
+  `registered_up_mod_img` tinyint(1) NOT NULL default '1',
+  `registered_del_img` tinyint(1) NOT NULL default '0',
+  `registered_create_mod_gal` tinyint(1) NOT NULL default '1',
+  `registered_del_gal` tinyint(1) NOT NULL default '0',
+  `registered_vote_view` tinyint( 1 ) NOT NULL default '1',
+  `registered_vote_vote` tinyint( 1 ) NOT NULL default '1',
+                break;
+
+
+            default:
+              ' Enquire messafe error !!'
+        }
+
 
 		return ColumnsProperties;
 	}
