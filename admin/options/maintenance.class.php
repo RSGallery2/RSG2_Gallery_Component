814fd17e7867cb6fe12cbae347891c63a1cabfa4
ThomasFinnern <noreplay@tomfinnern.de>
2016-05-31 12:41:37 +0200

    getImgThumb removed path from name

--- admin/options/maintenance.class.php
+++ admin/options/maintenance.class.php
@@ -35,7 +35,8 @@ class rsg2_maintenance {
     	}
     	$image = array_rand($imgname);
 
-    	$imgdata = getimagesize( imgUtils::getImgThumbPath($imgname[$image], true) );
+    	//$imgdata = getimagesize( imgUtils::getImgThumbPath($imgname[$image], true) );
+    	$imgdata = getimagesize( imgUtils::getImgThumb($imgname[$image], true) );
     	if ( $imgdata[0] == $rsgConfig->get('thumb_width') ) {
     		return false;
     	} else {
@@ -56,7 +57,8 @@ class rsg2_maintenance {
     	}
     	$image = array_rand($imgname);
 
-    	$imgdata = getimagesize( imgUtils::getImgDisplayPath($imgname[$image], true) );
+    	//$imgdata = getimagesize( imgUtils::getImgDisplayPath($imgname[$image], true) );
+    	$imgdata = getimagesize( imgUtils::getImgDisplay($imgname[$image], true) );
     	if ( $imgdata[0] == $rsgConfig->get('image_width') ) {
     		return false;
     	} else {
