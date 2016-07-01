ee49a1314464a1552bb6e8b3d24ffde016d04881
ThomasFinnern <noreplay@tomfinnern.de>
2016-07-01 12:37:18 +0200

    commit empty Letter case

--- admin/models/maintConsolidateDB.php
+++ admin/models/maintConsolidateDB.php
@@ -111,11 +111,12 @@ class rsgallery2ModelMaintConsolidateDB extends  JModelList
             catch (RuntimeException $e)
             {
                 $OutTxt = '';
-                $OutTxt .= 'Error executing query: "' . $query . '" in IsWatermarkActive' . '<br>';
+                $OutTxt .= 'Error executing query: "' . $query . '"' . '<br>';
                 $OutTxt .= 'Error: "' . $e->getMessage() . '"' . '<br>';
 
                 $app = JFactory::getApplication();
                 $app->enqueueMessage($OutTxt, 'error');
+                
             }
         }
 
