3b12cf367de18cae613eb864669c7008bd6c4bdc
Thomas.Finnern <GitProjects@insidethemachine.de>
2016-07-13 22:50:17 +0200

    Added search view

--- admin/views/acl_items/tmpl/default.php
+++ admin/views/acl_items/tmpl/default.php
@@ -36,7 +36,56 @@ $ListDirn = '';
 			<form action="<?php echo JRoute::_('index.php?option=com_rsgallery2&view=comments'); ?>"
 				  method="post" name="adminForm" id="adminForm" class="form-validate form-horizontal" >
 
-                <?php if (empty($this->items)) : ?>
+				<fieldset class="filter">
+					<div class="btn-toolbar">
+						<div class="btn-group">
+							<label for="filter_search">
+								<?php echo JText::_('JSEARCH_FILTER_LABEL'); ?>
+							</label>
+						</div>
+						<div class="btn-group">
+							<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" size="30" title="<?php echo JText::_('COM_CONTENT_FILTER_SEARCH_DESC'); ?>" />
+						</div>
+						<div class="btn-group">
+							<button type="submit" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_SUBMIT'); ?>" data-placement="bottom">
+								<span class="icon-search"></span><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+							<button type="button" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_CLEAR'); ?>" data-placement="bottom" onclick="document.getElementById('filter_search').value='';this.form.submit();">
+								<span class="icon-remove"></span><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
+						</div>
+					</div>
+					<hr class="hr-condensed" />
+					<div class="filters">
+						<select name="filter_access" class="input-medium" onchange="this.form.submit()">
+							<option value=""><?php echo JText::_('JOPTION_SELECT_ACCESS');?></option>
+							<?php echo JHtml::_('select.options', JHtml::_('access.assetgroups'), 'value', 'text', $this->state->get('filter.access'));?>
+						</select>
+
+						<select name="filter_published" class="input-medium" onchange="this.form.submit()">
+							<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></option>
+							<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.published'), true);?>
+						</select>
+
+						<?php if ($this->state->get('filter.forcedLanguage')) : ?>
+							<select name="filter_category_id" class="input-medium" onchange="this.form.submit()">
+								<option value=""><?php echo JText::_('JOPTION_SELECT_CATEGORY');?></option>
+								<?php echo JHtml::_('select.options', JHtml::_('category.options', 'com_content', array('filter.language' => array('*', $this->state->get('filter.forcedLanguage')))), 'value', 'text', $this->state->get('filter.category_id'));?>
+							</select>
+							<input type="hidden" name="forcedLanguage" value="<?php echo $this->escape($this->state->get('filter.forcedLanguage')); ?>" />
+							<input type="hidden" name="filter_language" value="<?php echo $this->escape($this->state->get('filter.language')); ?>" />
+						<?php else : ?>
+							<select name="filter_category_id" class="input-medium" onchange="this.form.submit()">
+								<option value=""><?php echo JText::_('JOPTION_SELECT_CATEGORY');?></option>
+								<?php echo JHtml::_('select.options', JHtml::_('category.options', 'com_content'), 'value', 'text', $this->state->get('filter.category_id'));?>
+							</select>
+							<select name="filter_language" class="input-medium" onchange="this.form.submit()">
+								<option value=""><?php echo JText::_('JOPTION_SELECT_LANGUAGE');?></option>
+								<?php echo JHtml::_('select.options', JHtml::_('contentlanguage.existing', true, true), 'value', 'text', $this->state->get('filter.language'));?>
+							</select>
+						<?php endif; ?>
+					</div>
+				</fieldset>
+
+				<?php if (empty($this->items)) : ?>
                     <div class="alert alert-no-items">
                         <?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
                     </div>
@@ -183,7 +232,24 @@ $ListDirn = '';
 				</table>
 
                 <?php endif;?>
-            </form>
+
+
+				<div>
+					<input type="hidden" name="task" value="" />
+					<input type="hidden" name="boxchecked" value="0" />
+					<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
+					<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
+
+					<?php echo JHtml::_('form.token'); ?>
+
+
+
+				</div>
+
+
+
+
+			</form>
 
         </div>
 
