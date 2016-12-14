7e5aea87462096bd434369ff0d3dbb7b555920d8
ThomasFinnern <noreplay@tomfinnern.de>
2016-12-14 12:58:46 +0100

    Added first part of createImageDbItem

--- admin/models/image.php
+++ admin/models/image.php
@@ -12,7 +12,7 @@ class Rsgallery2ModelImage extends  JModelAdmin
     protected $text_prefix = 'COM_RSGALLERY2';
 
 
-	/**
+    /**
 	 * Returns a reference to the a Table object, always creating it.
 	 *
 	 * @param       string $type    The table type to instantiate
@@ -21,7 +21,7 @@ class Rsgallery2ModelImage extends  JModelAdmin
 	 * @return      JTable  A database object
 	 * @since       2.5
 	 */
-	public function getTable($type = 'Image', $prefix = 'Rsgallery2Table', $config = array())
+	public function getTable($type = 'Image', $prefix = 'Rsgallery2Table', $config = array()) 
 	{
 		return JTable::getInstance($type, $prefix, $config);
 	}
@@ -34,13 +34,13 @@ class Rsgallery2ModelImage extends  JModelAdmin
 	 * @return      mixed   A JForm object on success, false on failure
 	 * @since       2.5
 	 */
-	public function getForm($data = array(), $loadData = true)
+	public function getForm($data = array(), $loadData = true) 
 	{
 		$options = array('control' => 'jform', 'load_data' => $loadData);
-		$form = $this->loadForm('com_rsgallery2.images', 'image',
+		$form = $this->loadForm('com_rsgallery2.images', 'image', 
 			array('control' => 'jform', 'load_data' => $loadData));
 
-		if (empty($form))
+		if (empty($form)) 
 		{
 			return false;
 		}
@@ -53,70 +53,70 @@ class Rsgallery2ModelImage extends  JModelAdmin
 	 * @return      mixed   The data for the form.
 	 * @since       2.5
 	 */
-	protected function loadFormData()
+	protected function loadFormData() 
 	{
 		// Check the session for previously entered form data.
 		$app = JFactory::getApplication();
 		$data = $app->getUserState('com_rsgallery2.edit.image.data', array());
-		if (empty($data))
+		if (empty($data)) 
 		{
 			$data = $this->getItem();
 		}
 		return $data;
 	}
 
-	// Transform some data before it is displayed ? Saved ?
-	/* extension development 129 bottom */
-	protected function prepareTable ($table)
-	{
-		/**
-		$table->name = htmlspecialchars_decode ($table->name, ENT_Quotes);
+    // Transform some data before it is displayed ? Saved ?
+    /* extension development 129 bottom */
+    protected function prepareTable ($table)
+    {
+/**
+        $table->name = htmlspecialchars_decode ($table->name, ENT_Quotes);
 
 		$table->generateAlias();
-		/**/
+/**/
 
-		$date = JFactory::getDate()->toSql();
+        $date = JFactory::getDate()->toSql();
 
-		$table->name = htmlspecialchars_decode ($table->name, ENT_QUOTES);
+        $table->name = htmlspecialchars_decode ($table->name, ENT_QUOTES);
 
-		// $table->generateAlias();
+        // $table->generateAlias();
 
-		if (empty($table->id))
-		{
-			// Set the values
-			$table->date = $date;
+        if (empty($table->id))
+        {
+            // Set the values
+            $table->date = $date;
 
-			/**
-			// Set ordering to the last item if not set
-			if (empty($table->ordering))
-			{
-			$db = $this->getDbo();
-			$query = $db->getQuery(true)
-			->select('MAX(ordering)')
-			->from($db->quoteName('#__rsgallery2_files'));
-			$db->setQuery($query);
-			$max = $db->loadResult();
+	        /**
+            // Set ordering to the last item if not set
+            if (empty($table->ordering))
+            {
+                $db = $this->getDbo();
+                $query = $db->getQuery(true)
+                    ->select('MAX(ordering)')
+                    ->from($db->quoteName('#__rsgallery2_files'));
+                $db->setQuery($query);
+                $max = $db->loadResult();
 
-			$table->ordering = $max + 1;
+                $table->ordering = $max + 1;
 
-			// Set the values
-			$table->date = $date;
-			$table->userid = JFactory::getUser()->id;
-			}
-			/**/
+                // Set the values
+                $table->date = $date;
+                $table->userid = JFactory::getUser()->id;
+            }
+	        /**/
 
-			$table->ordering = $table->getNextOrder('gallery_id = ' . (int) $table->gallery_id); // . ' AND state >= 0');
-		}
-		else
-		{
-			// Set the values
-			$table->date = $date;
-			$table->userid = JFactory::getUser()->id;
-		}
+	        $table->ordering = $table->getNextOrder('gallery_id = ' . (int) $table->gallery_id); // . ' AND state >= 0');
+        }
+        else
+        {
+            // Set the values
+            $table->date = $date;
+            $table->userid = JFactory::getUser()->id;
+        }
 
-		// Increment the content version number.
-		// $table->version++;
-	}
+        // Increment the content version number.
+        // $table->version++;
+    }
 
 	/**
 	 * A protected method to get a set of ordering conditions.
@@ -155,8 +155,8 @@ class Rsgallery2ModelImage extends  JModelAdmin
 
 		// Automatic handling of alias for empty fields
 		if (in_array($task, array('apply', 'save', 'save2new'))
-			// && (!isset($data['id']) || (int) $data['id'] == 0) // <== only for new item
-		)
+				// && (!isset($data['id']) || (int) $data['id'] == 0) // <== only for new item
+        )
 		{
 			if (empty ($data['alias']))
 			{
@@ -169,25 +169,25 @@ class Rsgallery2ModelImage extends  JModelAdmin
 					$data['alias'] = JFilterOutput::stringURLSafe($data['name']);
 				}
 
-				// check for existing alias
+                // check for existing alias
 				$table = $this->getTable();
 
 				//if ($table->load(array('alias' => $data['alias'], 'catid' => $data['catid'])))
-				// Warning on existing alias
+                // Warning on existing alias
 				if ($table->load(array('alias' => $data['alias'])))
 				{
 					$msg = JText::_('COM_RSGALLERY2_NAME_CHANGED_AS_WAS_EXISTING');
 				}
 
 				// Create unique alias and name
-				list($name, $alias) = $this->generateNewTitle(null, $data['alias'], $data['name']);
-				$data['alias'] = $alias;
-				$data['name'] = $name;
+                list($name, $alias) = $this->generateNewTitle(null, $data['alias'], $data['name']);
+                $data['alias'] = $alias;
+                $data['name'] = $name;
 
-				if (isset($msg))
-				{
-					JFactory::getApplication()->enqueueMessage($msg, 'warning');
-				}
+                if (isset($msg))
+                {
+                    JFactory::getApplication()->enqueueMessage($msg, 'warning');
+                }
 
 			}
 		}
@@ -199,11 +199,11 @@ class Rsgallery2ModelImage extends  JModelAdmin
 
 			if ($app->input->get('task') == 'save2copy')
 			{
-			// Reorder table so that new record has a unique ordering value
-			$table->load($new_pk);
-			$conditions_array = $this->getReorderConditions($table);
-			$conditions = implode(' AND ', $conditions_array);
-			$table->reorder($conditions);
+				// Reorder table so that new record has a unique ordering value
+				$table->load($new_pk);
+				$conditions_array = $this->getReorderConditions($table);
+				$conditions = implode(' AND ', $conditions_array);
+				$table->reorder($conditions);
 			}
 			/**/
 			return true;
@@ -265,98 +265,64 @@ class Rsgallery2ModelImage extends  JModelAdmin
 		return $name;
 	}
 
-	// ToDo: try to do it more elegant
+	// ToDo: try to do it more elegant 
 	// Called by maintenance -> Consolidate image database
 	// load table (? may init all varioables )
 	// Change what is necessary and use save see above
 
-	public function CreateImage ($imageName, $galleryId)
-	{
-		$HasError = 0;
+    public function createImageDbItem($imageName)
+    {
+        $IsImageDbCreated = 0;
 
-		$OutTxt = 'Model Image: CreateImage "' . $imageName . '""  galleryId' . $galleryId;
-		JFactory::getApplication()->enqueueMessage($OutTxt, 'warning');
-
-		/**
-
-		CREATE TABLE IF NOT EXISTS `#__rsgallery2_files` (
-		`id` int(9) unsigned NOT NULL auto_increment,
-		`name` varchar(255) NOT NULL default '',
-		`alias` varchar(255) NOT NULL DEFAULT '',
-		`descr` text,
-		`gallery_id` int(9) unsigned NOT NULL default '0',
-		`title` varchar(255) NOT NULL default '',
-		`hits` int(11) unsigned NOT NULL default '0',
-		`date` datetime NOT NULL default '0000-00-00 00:00:00',
-		`rating` int(10) unsigned NOT NULL default '0',
-		`votes` int(10) unsigned NOT NULL default '0',
-		`comments` int(10) unsigned NOT NULL default '0',
-		`published` tinyint(1) NOT NULL default '1',
-		`checked_out` int(11) NOT NULL default '0',
-		`checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
-		`ordering` int(9) unsigned NOT NULL default '0',
-		`approved` tinyint(1) unsigned NOT NULL default '1',
-		`userid` int(10) NOT NULL,
-		`params` text NOT NULL,
-		`asset_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'FK to the #__assets table.',
-
-		/**
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true);
-		$query->insert($db->quoteName('#__my_users'))
-		->columns(array('name', 'username'))
-		->values(implode(',', array($db->quote('Joe'), $db->quote('jlipman')) ));
-		$db->setQuery($query);
-		$result = $db->query();
-		/**/
-
-		/**
-		// Create and populate an object.
-		$image_record = new stdClass();
-		$image_record->name = 'Joel';
-		$image_record->username = 'jlipman';
-
-		// Insert the object into the user table.
-		$result = JFactory::getDbo()->insertObject('#__rsgallery2_files', $image_record);
-
-
-		/**/
-
+        $OutTxt = 'Model Image: CreateImage "' . $imageName . '"  ';
+        JFactory::getApplication()->enqueueMessage($OutTxt, 'notice');
 
 		$item = $this->getTable('');
 
 		$item->load(0);
 
-		$item->gallery_id= $galleryId;
-
-		$item->ordering = maxOrdering ($galleryId);;
+		// $item->gallery_id= $galleryId;
+		// $item->ordering = maxOrdering ($galleryId);;
 
 		$user = JFactory::getUser();
 		$userId = $user->id;
 		$item->userid  = $userId;
-		$item->title   = $imageName;
+
+        //----------------------------------------------------
+        // db: new image name
+        //----------------------------------------------------
+
+        // Create unique image file name
+        $oldName   = $imageName;
+        $item->name = $this->generateNewImageName($oldName);
+
+        // Create unique alias and title
+        list($title, $alias) = $this->generateNewTitle(null, $item->alias, $item->name);
+        $item->title = $title;
+        $item->alias = $alias;
+
 		$item->content = "";
-		$item->state   = 0; // "not published ??? published ?"$state;
+		$item->state   = 0; // "not published" -> maintenance consolidate db -> create image db from file name
 
 		// Lets store it!
-		$row             = JTable::getInstance('Message','BestiaTable');
-		// $row->bind($data);
-		$row->check();
-
+		$item->check();
 
 		if (!$item->store())
 		{
+            // toDO: collect erorrs and display over enque .... with errr type
+            $UsedNamesText = '<br>SrcImage: ' . $oldName . '<br>DstImage: ' . $item->name;
+            JFactory::getApplication()->enqueueMessage(JText::_('copied image name could not be inseted in database') . $UsedNamesText, 'warning');
+
+            $IsImageDbCreated = false;
+
 			$this->setError($this->_db->getErrorMsg());
+		} else {
 
-			return false;
-		}
+            $IsImageDbCreated = true;
+        }
 
-		$store           = $row->store();
-		if($store)
-			return $row->id;
-
-		return HasError;
-	}
+        return $IsImageDbCreated;
+    }
 
 	/**
 	 * moveImagesTo ()
@@ -373,7 +339,7 @@ class Rsgallery2ModelImage extends  JModelAdmin
 
 			$input = JFactory::getApplication()->input;
 			$cids = $input->get( 'cid', array(), 'ARRAY');
-			ArrayHelper::toInteger($cids);
+            ArrayHelper::toInteger($cids);
 
 			$NewGalleryId = $input->get( 'SelectGallery4MoveCopy', -1, 'INT');
 
@@ -382,22 +348,28 @@ class Rsgallery2ModelImage extends  JModelAdmin
 				// Source images selected ?
 				if (count($cids) > 0) {
 
-					$row = $this->getTable();
+					$item = $this->getTable();
 
-					// All selected images
+                    // All selected images
 					foreach ($cids as $cid) {
 
-						$row->load($cid);
+						$item->load($cid);
 
-						// Item is already in this gallery:
-						if ($row->gallery_id == $NewGalleryId) {
-							continue;
-						}
+                        // Item is already in this gallery:
+                        if ($item->gallery_id == $NewGalleryId) {
+                            continue;
+                        }
 
-						$row->gallery_id= $NewGalleryId;
-						$row->ordering = $this->maxOrdering ($NewGalleryId);
+						$item->gallery_id= $NewGalleryId;
+						$item->ordering = $this->maxOrdering ($NewGalleryId);
 
-						if (!$row->store())
+                        /**
+                        $user = JFactory::getUser();
+                        $userId = $user->id;
+                        $item->userid  = $userId;
+						/***/
+
+						if (!$item->store())
 						{
 							// toDO: collect erorrs and display over enque .... with errr type
 
@@ -407,10 +379,10 @@ class Rsgallery2ModelImage extends  JModelAdmin
 						}
 					}
 
-					// Success
-					$IsMoved = true;
+                    // Success
+                    $IsMoved = true;
 
-					JFactory::getApplication()->enqueueMessage(JText::_('Move is successful. Please check order of images in destination gallery'), 'notice');
+                    JFactory::getApplication()->enqueueMessage(JText::_('Move is successful. Please check order of images in destination gallery'), 'notice');
 				}
 				else
 				{
@@ -433,7 +405,6 @@ class Rsgallery2ModelImage extends  JModelAdmin
 
 		return $IsMoved;
 	}
-
 
 	public function maxOrdering ($GalleryId)
 	{
@@ -486,15 +457,15 @@ class Rsgallery2ModelImage extends  JModelAdmin
 				// Source images selected ?
 				if (count($cids) > 0)
 				{
-					$row = $this->getTable();
+					$item = $this->getTable();
 
 					// All selected images
 					foreach ($cids as $cid)
 					{
-						$row->load($cid);
+						$item->load($cid);
 
 						/* Item is already in this gallery:
-						if ($row->gallery_id == $NewGalleryId)
+						if ($item->gallery_id == $NewGalleryId)
 						{
 							JFactory::getApplication()->enqueueMessage(
 								JText::_('Display image could not be copied. It is already assigned to the destination gallery') . $row->title, 'warning');
@@ -509,13 +480,13 @@ class Rsgallery2ModelImage extends  JModelAdmin
 						//----------------------------------------------------
 
 						// Create unique image file name
-						$oldName   = $row->name;
-						$row->name = $this->generateNewImageName($oldName);
+						$oldName   = $item->name;
+						$item->name = $this->generateNewImageName($oldName);
 
 						// Create unique alias and title
-						list($title, $alias) = $this->generateNewTitle(null, $row->alias, $row->name);
-						$row->title = $title;
-						$row->alias = $alias;
+						list($title, $alias) = $this->generateNewTitle(null, $item->alias, $item->name);
+						$item->title = $title;
+						$item->alias = $alias;
 
 						//----------------------------------------------------
 						// Copy files
@@ -524,7 +495,7 @@ class Rsgallery2ModelImage extends  JModelAdmin
 						// copy original
 						$fullPath_original = JPATH_ROOT . $rsgConfig->get('imgPath_original') . '/';
 						$srcFile           = $fullPath_original . $oldName;
-						$dstFile           = $fullPath_original . $row->name;
+						$dstFile           = $fullPath_original . $item->name;
 						if (!copy($srcFile, $dstFile))
 						{
 							// toDo: what todo if it fails ?
@@ -540,7 +511,7 @@ class Rsgallery2ModelImage extends  JModelAdmin
 						// must function !!!
 						$fullPath_display = JPATH_ROOT . $rsgConfig->get('imgPath_display') . '/';
 						$srcFile          = $fullPath_display . $oldName . '.jpg';
-						$dstFile          = $fullPath_display . $row->name . '.jpg';
+						$dstFile          = $fullPath_display . $item->name . '.jpg';
 						if (!copy($srcFile, $dstFile))
 						{
 							// toDo: what todo if it fails ?
@@ -557,7 +528,7 @@ class Rsgallery2ModelImage extends  JModelAdmin
 						// copy thumb
 						$fullPath_thumb = JPATH_ROOT . $rsgConfig->get('imgPath_thumb') . '/';
 						$srcFile        = $fullPath_thumb . $oldName . '.jpg';
-						$dstFile        = $fullPath_thumb . $row->name . '.jpg';
+						$dstFile        = $fullPath_thumb . $item->name . '.jpg';
 						if (!copy($srcFile, $dstFile))
 						{
 							// toDo: what todo if it fails ?
@@ -569,14 +540,14 @@ class Rsgallery2ModelImage extends  JModelAdmin
 						// db: insert new item
 						//----------------------------------------------------
 
-						$row->gallery_id = $NewGalleryId;
-						$row->ordering   = $this->maxOrdering($NewGalleryId);
-						$row->id         = 0; // it is new item
+						$item->gallery_id = $NewGalleryId;
+						$item->ordering   = $this->maxOrdering($NewGalleryId);
+						$item->id         = 0; // it is new item
 
-						if (!$row->store())
+						if (!$item->store())
 						{
 							// toDO: collect erorrs and display over enque .... with errr type
-							$UsedNamesText = '<br>SrcImage: ' . $oldName . '<br>DstImage: ' . $row->name;
+							$UsedNamesText = '<br>SrcImage: ' . $oldName . '<br>DstImage: ' . $item->name;
 							JFactory::getApplication()->enqueueMessage(JText::_('copied image name could not be inseted in database') . $UsedNamesText, 'warning');
 
 							$this->setError($this->_db->getErrorMsg());
@@ -654,37 +625,5 @@ class Rsgallery2ModelImage extends  JModelAdmin
 
     }
 	/**/
-
-	/**
-	 * copy the item to an other gallery
-	 * @param int id of the target gallery
-	 * @return rsgItem newly created rsgItem
-	 *
-
-	              function copy($target_gallery){
-
-	if($target_gallery == null) return null;
-
-	global $database,$rsgConfig;
-
-	$new_item = clone($this);
-	$new_item->gallery_id = $target_gallery;
-
-	if( !$database->insertObject('#__rsgallery2_files', $new_item, 'id') ) {
-	$this->setError( $database->getErrorMsg() );
-	return null;
-	}
-
-	if ( $rsgConfig->get('gallery_folders') ){
-
-	// TODO: copy files from source to target gallery folder
-
-	}
-
-	return $this;
-
-	}
-	/**/
-
 
 }
