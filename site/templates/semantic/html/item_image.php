cd4b5c43636afff9a023f0b81bf4a5f68206e603
ThomasFinnern <github@tomfinnern.de>
2016-12-25 18:01:31 +0100

    Fixed: modal popup was not working any more

--- site/templates/semantic/html/item_image.php
+++ site/templates/semantic/html/item_image.php
@@ -17,52 +17,35 @@ global $rsgConfig;
 	switch ($rsgConfig->get('displayPopup')) {
 		//No popup
 		case 0:{
-					?>
-					<img class="rsg2-displayImage" src="<?php echo $imageUrl;?>" alt="<?php echo $item->name; ?>" title="<?php echo $item->name; ?>" />
-		<?php
-		break;
-	}
-	//Normal popup
-	case 1:{
-					?><a href="<?php echo $imageOriginalUrl; ?>" target="_blank">
-					<img class="rsg2-displayImage" src="<?php echo $imageUrl;?>" alt="<?php echo $item->name; ?>" title="<?php echo $item->name; ?>" />
-					</a>
+            ?>
+            <img class="rsg2-displayImage" src="<?php echo $imageUrl;?>" alt="<?php echo $item->name; ?>" title="<?php echo $item->name; ?>" />
+    		<?php
+            break;
+        }
+        //Normal popup
+        case 1:{
+            ?>
+            <a href="<?php echo $imageOriginalUrl; ?>" target="_blank">
+                <img class="rsg2-displayImage" src="<?php echo $imageUrl;?>" alt="<?php echo $item->name; ?>" title="<?php echo $item->name; ?>" />
+            </a>
 			<?php
-			break;
+
+            break;
 		}
 		case 2:{
+            // ToDo: Place it on start ???
 			JHTML::_('behavior.modal');
-		$jsModal = '
-				window.addEvent("domready", function() {
-				SqueezeBox.initialize({});
-				var img = $$("img.rsg2-displayImage")[0];
-				img.addEvent("click", function(e)
-				{
-				new Event(e).stop();
-				SqueezeBox.fromElement(img,{url:"' . $imageOriginalUrl .'", 
-					classWindow:"rsg2", 
-					classOverlay:"rsg2",
-					onOpen:function(img){
-					var p = new Element("p", {class:"rsg2-popup-title"});
-					$(p).appendText("'. strip_tags($item->name) . '");
-					$(p).inject(img);
-					var pSize1 = $(p).getSize().size;
-					p = new Element("p", {class:"rsg2-popup-description"});
-					$(p).appendText("'. strip_tags($item->descr) . '");
-					$(p).inject(img);
-					var pSize2 = $(p).getSize().size;
-					size = SqueezeBox.size;
-					size.y += pSize1.y + pSize2.y;
-					SqueezeBox.resize(size, true);
-					}});		
-					});
-					});';
-			
-					?>
-					<img class="rsg2-displayImage" src="<?php echo $imageUrl;?>" alt="<?php echo $item->name; ?>" title="<?php echo $item->name; ?>" />
-			<?php
+			?>
+
+            <a class="modal" href="<?php echo $imageOriginalUrl; ?>">
+                <img class="rsg2-displayImage" src="<?php echo $imageUrl;?>" alt="<?php echo $item->name; ?>" title="<?php echo $item->name; ?>" />
+            </a>
+
+            <?php
+            /**
 			$doc = JFactory::getDocument();
 			$doc->addScriptDeclaration($jsModal);
+            /**/
 			break;
 		}
 	}
