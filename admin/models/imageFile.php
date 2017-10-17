73d8a5e2a8a4cd6891d5224811cddd87d3e837a9
Thomas.Finnern <github@tomfinnern.de>
2017-10-17 23:29:31 +0200

    Moved file handling parts from model image.php to imageFile.php

--- admin/models/imageFile.php
+++ admin/models/imageFile.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * @package     RSGallery2
- * @subpackage  com_rsgallery2
+ * @package         RSGallery2
+ * @subpackage      com_rsgallery2
  * @copyright   (C) 2016 - 2017 RSGallery2
- * @license     http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @author      finnern
+ * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @author          finnern
  * RSGallery is Free Software
  */
 
 defined('_JEXEC') or die;
 
-//require_once JPATH_COMPONENT_ADMINISTRATOR . '/includes/ExtImgLibAbstract.php';
+// Loaded with class require_once JPATH_COMPONENT_ADMINISTRATOR . '/includes/ExtImgLibAbstract.php';
 
 /**
  * Single image model
@@ -32,33 +32,36 @@ class Rsgallery2ImageFile
 	 *
 	 * @since 4.3.0
 	 */
-	public function __construct($NewImageLib=null)
+	public function __construct($NewImageLib = null) // ToDo: Check if the name shall be given instead of libry object
 	{
 		global $rsgConfig;
 
-		// Image library is given
-		if( ! empty ($NewImageLib))
+		// ToDo: try catch
+		// ToDo: ? fallback when lib is not existing any more ?
+
+		// Image library is already defined (given by caller)
+		if (!empty ($NewImageLib))
 		{
 			$this->ImageLib = $NewImageLib;
 		}
 		else
 		{
 			// Use rsgConfig to determine which image library to load
-			$graphicsLib    = $rsgConfig->get('graphicsLib');
+			$graphicsLib = $rsgConfig->get('graphicsLib');
 			switch ($graphicsLib)
 			{
 				case 'gd2':
-					// return GD2::resizeImage($source, $target, $targetWidth);
+					// return GD
 					require_once JPATH_COMPONENT_ADMINISTRATOR . '/includes/ExtImgLib_GD.php';
 					$this->ImageLib = new external_GD2;
 					break;
 				case 'imagemagick':
-					//return ImageMagick::resizeImage($source, $target, $targetWidth);
+					//return ImageMagick
 					require_once JPATH_COMPONENT_ADMINISTRATOR . '/includes/ExtImgLib_imagemagick.php';
 					$this->ImageLib = new external_imagemagick;
 					break;
 				case 'netpbm':
-					//return Netpbm::resizeImage($source, $target, $targetWidth);
+					//return Netpbm
 					require_once JPATH_COMPONENT_ADMINISTRATOR . '/includes/ExtImgLib_netpbm.php';
 					$this->ImageLib = new external_netpbm;
 					break;
@@ -70,14 +73,267 @@ class Rsgallery2ImageFile
 
 					return false;
 			}
-
 		}
-
-
-
-
-
 	}
 
+	/*******************************************************************************************************
+	 *
+	 * !!! Moved from other file !!!
+	 *
+	 * ==> IMG
+	 *******************************************************************************************************/
+
+
+	/**
+	 * Creates a display image with size from config
+	 *
+	 * @param $originalFileName includes path (May be todifferent path then original)
+	 *
+	 * @return bool  true if successful
+	 *
+	 * @since 4.3.0
+	 */
+	public function createDisplayImageFile($originalFileName)
+	{
+		global $rsgConfig;
+		global $Rsg2DebugActive;
+
+		$IsImageCreated = false;
+
+		try
+		{
+			$baseName    = basename($originalFileName);
+			$imgSrcPath = JPATH_ROOT . $rsgConfig->get('imgPath_original') . '/' . $baseName;
+			$imgDstPath = JPATH_ROOT . $rsgConfig->get('imgPath_display') . '/' . $baseName . '.jpg';
+
+			if ($Rsg2DebugActive)
+			{
+				JLog::add('==> createDisplayFile: "' . $imgSrcPath . '" -> "' . $imgDstPath . '"');
+			}
+
+			$width  = getimagesize($imgSrcPath);
+			$height = $width[1];
+			$width  = $width[0];
+			if ($height > $width)
+			{
+				$maxSideImage = $height;
+			}
+			else
+			{
+				$maxSideImage = $width;
+			}
+
+			$userWidth = $rsgConfig->get('image_width');
+
+			// if original is wider or higher than display size, create a display image
+			if ($maxSideImage > $userWidth)
+			{
+				$IsImageCreated = $this->ImageLib->resizeImage($imgSrcPath, $imgDstPath, $userWidth);
+			}
+			else
+			{
+				$IsImageCreated = $this->ImageLib->resizeImage($imgSrcPath, $imgDstPath, $maxSideImage);
+			}
+		}
+		catch (RuntimeException $e)
+		{
+			$OutTxt = '';
+			$OutTxt .= 'Error executing createDisplayImageFile for image name: "' . $originalFileName . '"<br>';
+			$OutTxt .= 'Error: "' . $e->getMessage() . '"' . '<br>';
+
+			$app = JFactory::getApplication();
+			$app->enqueueMessage($OutTxt, 'error');
+		}
+
+		return $IsImageCreated;
+	}
+
+	/**
+	 * Generic image resize function
+	 * Uses in config defined grafic library
+	 *
+	 * @param string $imgSrcPath  full path of source image
+	 * @param string $imgDstPath  full path of target image
+	 * @param int    $targetWidth width of target
+	 *
+	 * @return bool true if successful
+	 *
+	 * @since 4.3.0
+	 *
+	 * public function resizeImage($imgSrcPath, $imgDstPath, $targetWidth)
+	 * {
+	 * $IsImageCreated = false;
+	 *
+	 * try
+	 * {
+	 * $IsImageCreated = $this->ImageLib->resizeImage($imgSrcPath, $imgDstPath, $targetWidth);
+	 * }
+	 * catch (RuntimeException $e)
+	 * {
+	 * $OutTxt = '';
+	 * $OutTxt .= 'Error executing imageFile::resizeImage "'
+	 * . '" for image source: "' . $imgSrcPath . '"<br>'
+	 * . '" for image desti.: "' . $imgDstPath . '"<br>';
+	 * $OutTxt .= 'Error: "' . $e->getMessage() . '"' . '<br>';
+	 *
+	 * $app = JFactory::getApplication();
+	 * $app->enqueueMessage($OutTxt, 'error');
+	 * }
+	 *
+	 * return $IsImageCreated;
+	 * }
+	 * /**/
+
+	/**
+	 * Creates a thumb image with size from config
+	 *
+	 * @param $originalFileName
+	 *
+	 * @return bool true if successful
+	 *
+	 * @since 4.3.0
+	 */
+	public function createThumbImageFile($originalFileName)
+	{
+		global $rsgConfig;
+		global $Rsg2DebugActive;
+
+		$IsImageCreated = false;
+
+		try
+		{
+			$thumbWidth = $rsgConfig->get('thumb_width');
+
+			$baseName    = basename($originalFileName);
+			$imgSrcPath = JPATH_ROOT . $rsgConfig->get('imgPath_original') . '/' . $baseName;
+			$imgDstPath = JPATH_ROOT . $rsgConfig->get('imgPath_thumb') . '/' . $baseName . '.jpg';
+
+			if ($Rsg2DebugActive)
+			{
+				JLog::add('==> createThumbImageFile: "' . $imgSrcPath . '" -> "' . $imgDstPath . '"');
+			}
+
+// ??			$IsImageCreated = $this->ImageLib->createSquareThumb($imgSrcPath, $imgDstPath, $thumbWidth);
+
+			// Is thumb style square // ToDo: Thumb style -> enum  // ToDo: general: Config enums
+			if ($rsgConfig->get('thumb_style') == 1)
+			{
+				$IsImageCreated = $this->ImageLib->createSquareThumb($imgSrcPath, $imgDstPath, $thumbWidth);
+			}
+			else
+			{
+				$IsImageCreated = $this->ImageLib->resizeImage($imgSrcPath, $imgDstPath, $thumbWidth);
+			}
+		}
+		catch (RuntimeException $e)
+		{
+			$OutTxt = '';
+			$OutTxt .= 'Error executing createThumbImageFile for image name: "' . $originalFileName . '"<br>';
+			$OutTxt .= 'Error: "' . $e->getMessage() . '"' . '<br>';
+
+			$app = JFactory::getApplication();
+			$app->enqueueMessage($OutTxt, 'error');
+		}
+
+		return $IsImageCreated;
+	}
+
+
+	public function moveFile2OriginalDir($srcFileName)
+	{
+		global $rsgConfig;
+		global $Rsg2DebugActive;
+
+		$isMoved = false;
+
+		// ToDo: if (JFile::exists(JPATH_DISPLAY . '/' . $basename) || JFile::exists(JPATH_ORIGINAL . '/' . $basename)) {
+		try
+		{
+			$dstFileName = JPATH_ROOT . $rsgConfig->get('imgPath_original') . '/' . basename($srcFileName);
+
+			if ($Rsg2DebugActive)
+			{
+				JLog::add('==> moveFile2OrignalDir: "' . $srcFileName . '"');
+			}
+
+			$isMoved = rename($srcFileName, $dstFileName);
+		}
+		catch (RuntimeException $e)
+		{
+			$OutTxt = '';
+			$OutTxt .= 'moveFile2OrignalDir: "' . $srcFileName . '" -> "' . $dstFileName . '"<br>';
+			$OutTxt .= 'Error: "' . $e->getMessage() . '"' . '<br>';
+
+			$app = JFactory::getApplication();
+			$app->enqueueMessage($OutTxt, 'error');
+		}
+
+		return $isMoved;
+	}
+
+	/*------------
+
+		ToDo: create image libs ... and use / create following functions then
+
+		ToDo: use ImgFile.php fur sub classes from here
+
+	-------------*/
+
+	/** other file class watermrk ..
+	public function createWaterMarkImageFile($originalFileName)
+	{
+		global $rsgConfig;
+		global $Rsg2DebugActive;
+
+		$isCreated = false;
+
+		// if (JFile::exists(JPATH_DISPLAY . '/' . $basename) || JFile::exists(JPATH_ORIGINAL . '/' . $basename)) {
+		try
+		{
+			$ImageLib = $this->ImageLib;
+
+			// ToDo: make separate functions in each grafics lib
+			// Actual short cut : use GD
+			// Use rsgConfig to determine which image library is loaded
+			$graphicsLib = $rsgConfig->get('graphicsLib');
+			// Use GD even if $graphicsLib is different
+			if ($graphicsLib != 'gd2')
+			{
+				require_once JPATH_COMPONENT_ADMINISTRATOR . '/includes/ExtImgLib_GD.php';
+				$ImageLib = new external_GD2;
+			}
+
+
+			$IsImageCreated = $ImageLib->resizeImage($imgSrcPath, $imgDstPath, $maxSideImage);
+
+
+				$baseName    = basename($originalFileName);
+			$srcFileName = JPATH_ROOT . $rsgConfig->get('imgPath_original') . '/' . $baseName;
+			$dstFileName = JPATH_ROOT . $rsgConfig->get('imgPath_watermarked') . '/' . $baseName;
+
+			if ($Rsg2DebugActive)
+			{
+				JLog::add('==> createWatermarkFile: "' . $srcFileName . '" -> "' . $dstFileName . '"');
+			}
+
+
+			// seed is used ...
+			// todo: copy and resize ...
+
+			$isCreated = copy($srcFileName, $dstFileName);
+		}
+		catch (RuntimeException $e)
+		{
+			$OutTxt = '';
+			$OutTxt .= 'createThumbFile: "' . $srcFileName . '"<br>';
+			$OutTxt .= 'Error: "' . $e->getMessage() . '"' . '<br>';
+
+			$app = JFactory::getApplication();
+			$app->enqueueMessage($OutTxt, 'error');
+		}
+
+		return $isCreated;
+	}
+	/**/
 
 }
