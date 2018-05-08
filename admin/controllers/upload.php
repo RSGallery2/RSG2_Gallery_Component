b29e92c288310babe247b51160781c2b135865fb
Thomas.Finnern <github@tomfinnern.de>
2018-05-08 12:41:24 +0200

    Fixed: Title did contain extension part of name accidental

--- admin/controllers/upload.php
+++ admin/controllers/upload.php
@@ -1180,7 +1180,7 @@ class Rsgallery2ControllerUpload extends JControllerForm
 			$title = $baseName;
 			$description = '';
 
-			$imgId = $modelDb->createImageDbItem($useFileName, $title, $galleryId, $description);
+			$imgId = $modelDb->createImageDbItem($useFileName, '', $galleryId, $description);
 			if (empty($imgId))
 			{
 				// actual give an error
