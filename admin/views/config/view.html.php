d649182d278886c06870d55a3784a64e2ceed9a1
Thomas.Finnern <github@tomfinnern.de>
2018-07-28 15:34:03 +0200

    spelling corrected

--- admin/views/config/view.html.php
+++ admin/views/config/view.html.php
@@ -58,7 +58,7 @@ class Rsgallery2ViewConfig extends JViewLegacy
 		// on develop show open tasks if existing
 		if (!empty ($Rsg2DevelopActive))
 		{
-			echo '<span style="color:red">Task: Save should not leave programm, rename save in controller as it is used by Raw ...</span><br><br>';
+			echo '<span style="color:red">Task: Save should not leave program, rename save in controller as it is used by Raw ...</span><br><br>';
 		}
 
 		//--- get needed form data ------------------------------------------
