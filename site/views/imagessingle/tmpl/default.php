7e4093d99da3dd96a4458d388df13b72dfa5cde9
Thomas.Finnern <github@tomfinnern.de>
2018-09-30 22:20:17 +0200

    Added new views

---
+++ site/views/imagessingle/tmpl/default.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package    [PACKAGE_NAME]
+ *
+ * @author     [AUTHOR] <[AUTHOR_EMAIL]>
+ * @copyright  [COPYRIGHT]
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       [AUTHOR_URL]
+ */
+
+//use Joomla\CMS\HTML\HTMLHelper;
+//use Joomla\CMS\Layout\FileLayout;
+
+defined('_JEXEC') or die;
+
+//$layoutGallery = JLayoutHelper::render('.', $myLayoutData);
+echo "--- Before render image single ---------------<br>";
+//$layout = new JLayoutFile('ClassicJ25.gallery', null, array('debug' => true, 'client' => 1)); // , null, array('debug' => true, 'client' => 1, 'component' => 'com_tags')
+//$layout = new JLayoutFile('ClassicJ25.gallery');
+//echo $layout->render($galleryData);
+//echo "---After Render ---------------<br>";
+
