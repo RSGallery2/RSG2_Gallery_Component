64d7cd18f67eb279021aeaf28afd849ed5d1853e
ThomasFinnernVM <github@tomfinnern.de>
2018-10-25 13:08:21 +0200

    Added output to View

--- site/layouts/ClassicJ25/gallery.php
+++ site/layouts/ClassicJ25/gallery.php
@@ -30,23 +30,74 @@ echo "<br>layout classic: gallery end <br>";
 //echo "<br>";
 /**/
 
-$gallery = $displayData['gallery'];
-echo '<h3>' . $gallery->name . '</h3>';
+echo '<div class="rsg2">';
 
-/**/
+$gallery = $displayData['gallery'];
+echo '<hr>';
+echo '<h3>' . $gallery->name . '</h3><br>';
+echo '<div class="intro_text">';
+echo    $rsgConfig->get('intro_text');
+echo '</div>';
+echo '<hr>';
+
+echo '<div class="rsg2-clr"/>';
+
+/**
 $imagesJson = json_encode($displayData['images']);
 echo "Images: " . $imagesJson;
 /**/
 
 $images = $displayData['images'];
+//$rowLimit = $rsgConfig->rowlimit;
+$rowLimit = 3;
 
-foreach ($images as $image)
+echo '<table id="rsg2-thumbsList" border="0">';
+echo '   <tbody>';
+
+foreach ($images as $idx=>$image)
 {
+	// Start new row
+	if ($idx  % $rowLimit == 0)
+	{
+		echo '      <tr>';
+
+	}
 	echo "<h4>" . $image->name . "</h4>";
-	
+
+	echo '			<td>';
+	echo '				<div class="shadow-box">';
+		echo '					<div class="img-shadow">';
+		echo '						<a href="/Joomla3x/index.php/gallery-test/item/1925/asInline">';
+		//v falsch <img src="http://127.0.0.1/joomla3x/0DSC_5504-3.JPG" alt="">
+		//echo '							<img src="http://127.0.0.1/Joomla3x/images/rsgallery/thumb/2006-10-05_00002.jpg.jpg" alt="">';
+		echo '<img src="' . $image->UrlThumbFile . '" alt="" />';
+		echo '						</a>';
+		echo '					</div>';
+		echo '				</div>';
+		echo '				<div class="rsg2-clr"/>';
+		echo '				<br>';
+		echo '				<span class="rsg2_thumb_name">';
+		echo '		2006-10-05_00002';
+		echo '		</span>';
+		echo '				<br>';
+		echo '				<i/>';
+		echo '			</td>';
 	echo '<img src="' . $image->UrlThumbFile . '" alt="" />';
 	echo "<br>";
+
+	// Start new row
+	if (($idx + 1)% $rowLimit == 0)
+	{
+		echo '      <\tr>';
+	}
 }
+echo '   </tbody>';
+echo '</table>';
+
+
+echo '<\\div>'; // <div class="rsg2">
+
+
 
 ?>
 
