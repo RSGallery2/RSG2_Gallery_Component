855fa78ca51cf7c3e7562e51904373e98fe1fc9a
ThomasFinnernVM <github@tomfinnern.de>
2019-03-12 13:02:45 +0100

    Test reminder

--- admin/views/upload/view.html.php
+++ admin/views/upload/view.html.php
@@ -166,8 +166,8 @@ class Rsgallery2ViewUpload extends JViewLegacy
         {
 	        echo '<span style="color:red">'
                 . 'Tasks: <br>'
+				. '!!! Test filenames with blanks and other ... '
 				. '*  <br>??? enable/force selection of start gallery ???'
-//				. '*  <br>'
 //				. '*  <br>'
 //				. '*  <br>'
 //				. '*  <br>'
