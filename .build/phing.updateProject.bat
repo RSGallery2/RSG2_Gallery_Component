ebeef5b35a82833ec9c5df9cd8fbf3b986029da4
ThomasFinnernVM <github@tomfinnern.de>
2019-06-21 13:33:38 +0200

    Improved calling batches

--- .build/phing.updateProject.bat
+++ .build/phing.updateProject.bat
@@ -1,10 +1,10 @@
 @ECHO off
-REM starts the standard build process
+REM Update actual project 
 
 REM phing -verbose -debug -logfile .\build.log .\build.xml
 REM phing -verbose -logfile .\build.log .\build.xml
 REM phing -logfile .\build.log .\build.xml
-phing -logfile .\build.log 
+phing -logfile .\build.log -f .\updateProject.xml
 
 
 
