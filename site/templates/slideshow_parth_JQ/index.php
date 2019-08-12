450eff568110a79b7cf446684b7eeea5b5e02a4a
ThomasFinnernVM <github@tomfinnern.de>
2019-08-12 13:22:17 +0200

    Prepared develop of Parth slider with jquery

---
+++ site/templates/slideshow_parth_JQ/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Prep for slideshow_parth
+ *
+ * @package       RSGallery2
+ * @copyright (C) 2003 - 2019 RSGallery2
+ * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ *                RSGallery is Free Software
+ */
+
+defined('_JEXEC') or die();
+
+// bring in display code
+$templatePath = JPATH_RSGALLERY2_SITE . '/templates' . '/slideshow_parth';
+require_once($templatePath . '/display.class.php');
+
+$rsgDisplay = new rsgDisplay_slideshow_parth();
+
+$rsgDisplay->showSlideShow();
