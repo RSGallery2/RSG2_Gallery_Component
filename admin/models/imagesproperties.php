2d97a4edb799b561c674888df198fe63f32c0bbe
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2021-02-18 20:52:06 +0100

    copyright date

--- admin/models/imagesproperties.php
+++ admin/models/imagesproperties.php
@@ -2,7 +2,7 @@
 /**
  * @package     RSGallery2
  * @subpackage  com_rsgallery2
- * @copyright   (C) 2016-2020 RSGallery2 Team
+ * @copyright   (C) 2016-2021 RSGallery2 Team
  * @license     http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @author      finnern
  * RSGallery is Free Software
@@ -185,7 +185,6 @@ class Rsgallery2ModelImagesProperties extends JModelList
 			// identify active file
 			JLog::add('cids: "' . json_encode($cids));
 		}
-
 
 		if (is_array ($cids))
 		{
